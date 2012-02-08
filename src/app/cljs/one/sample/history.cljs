045ecf856f8c72f3d4651c7a8797d255b185d3a8
Murphy McMahon <pandeiro@gmail.com>
2012-02-08 10:13:09 -0200

    no longer using one.browser.history due to gclosure bug

--- src/app/cljs/one/sample/history.cljs
+++ src/app/cljs/one/sample/history.cljs
@@ -1,23 +1,33 @@
-(ns ^{:doc "When this library is loaded, a listener function is added
-  which will be run when a :form or :greeting event is fired. This
-  allows the use of the back button to navigate between views. This is
-  accomplished by using library.browser.history to keep track of views
-  that have previously been visited, and traversing them when
-  navigation events are detected."}
+(ns ^{:doc "This module no longer uses the one.browser.history library due
+  to a buggy implementation of Html5History in the Google Closure library
+  which causes duplicate events to fire (one for hashchange and another for
+  popstate, both of which are involved in any token change). Its replacement
+  is a simple event listener on the hashchange event to detect and
+  dispatch a new token."}
   one.sample.history
-  (:require [one.dispatch :as dispatch]
-            [one.browser.history :as history]))
+  (:require [one.dispatch        :as dispatch]
+            [one.browser.history :as history]
+            [goog.events         :as event]))
 
-(defn nav-handler
-  "Handle navigation events by firing the appropriate view token."
-  [{:keys [token navigation?]}]
-  (when navigation?
-    (dispatch/fire token)))
+(event/listen js/window goog.events.EventType.HASHCHANGE
+              (fn [e]
+                (if (not= (.-oldURL e) (.-newURL e))
+                  (let [link  (.createElement js/document "a")
+                        url   (set! (.-href link) (.-newURL e))
+                        token (.substr (.-hash link) 1)]
+                    (dispatch/fire (keyword token))))))
 
-(def ^{:doc "The global history object for this application."}
-  history (history/history nav-handler))
+(comment
+  (defn nav-handler
+    "Handle navigation events by firing the appropriate view token."
+    [{:keys [token navigation?]}]
+    (when navigation?
+      (dispatch/fire token)))
 
-(comment (dispatch/react-to #{:init}
-                            (fn [t d]
-                              (history/set-token history t))))
+  (def ^{:doc "The global history object for this application."}
+    history (history/history nav-handler))
+
+  (dispatch/react-to #{:init}
+                     (fn [t d]
+                       (history/set-token history t))))
 

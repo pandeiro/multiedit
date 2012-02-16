de7ed3f842d7aee1b9e47929a469d6d4bde3caec
Murphy McMahon <pandeiro@gmail.com>
2012-02-16 17:13:29 -0200

    accept keyword as selector and return seq instead of array for queries matching >1 nodes

--- src/lib/cljs/query.cljs
+++ src/lib/cljs/query.cljs
@@ -1,11 +1,12 @@
 (ns query)
 
 (defn $
-  "A basic query selector similar to Sizzle/jQuery. Uses document.querySelectorAll(),
+  "Basic query selector similar to Sizzle/jQuery. Uses document.querySelectorAll(),
   only available in modern browsers"
   ([selector] ($ selector js/document))
   ([selector ctx]
-     (let [result (. ctx (querySelectorAll selector))]
+     (let [selector (name selector)
+           result   (. ctx (querySelectorAll selector))]
        (if (and result (= 1 (.-length result)))
          (aget result 0)
-         result))))
+         (array-seq result 0)))))

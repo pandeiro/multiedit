c0ea860da1da41664df1a862a253c3c51a70d46c
Murphy McMahon <pandeiro@gmail.com>
2012-02-23 19:07:11 -0200

    add storage event listener, change fn name to load-local-docs, erase old docstring from action

--- src/app/cljs/one/sample/controller.cljs
+++ src/app/cljs/one/sample/controller.cljs
@@ -3,36 +3,22 @@
   one.sample.controller
   (:use [one.sample.model :only (state docs uuid)])
   (:require [cljs.reader        :as reader]
+            [goog.events        :as event]
             [one.dispatch       :as dispatch]
             [one.sample.history :as history]
             [local              :as local]))
 
-(defmulti action
-  "Accepts a map containing information about an action to perform.
-
-  Actions may cause state changes on the client or the server. This
-  function dispatches on the value of the `:type` key and currently
-  supports `:init`, `:form`, and `:greeting` actions.
-
-  The `:init` action will initialize the appliation's state.
-
-  The `:form` action will only update the status atom, setting its state
-  to `:from`.
-
-  The `:greeting` action will send the entered name to the server and
-  update the state to `:greeting` while adding `:name` and `:exists`
-  values to the application's state."
-  :type)
+(defmulti action :type)
 
 (defn authenticate [callback]
   (callback (if (> 6 (rand 10)) "pablo" nil)))
 
-(defn check-for-local-docs []
+(defn load-local-docs []
   (if-let [local-docs (local/get-clojure "docs")]
     (reset! docs local-docs)))
 
 (defmethod action :init []
-  (check-for-local-docs)
+  (load-local-docs)
   (authenticate (fn [who]
                   (reset! state {:state :init :who who})
                   (let [token (history/get-token)]
@@ -64,3 +50,8 @@
 
 (dispatch/react-to token-changed-to-id?
                    (fn [t d] (get-document (name t))))
+
+(event/listen js/window
+              "storage"
+              (fn [e]
+                (dispatch/fire :storage-updated)))

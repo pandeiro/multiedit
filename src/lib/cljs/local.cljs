20c531eeed3e682673c56dcbb4a6197f72d71f06
Murphy McMahon <pandeiro@gmail.com>
2012-02-24 16:14:55 -0200

    allows keywords as localStorage keys and updates method call to new syntax

--- src/lib/cljs/local.cljs
+++ src/lib/cljs/local.cljs
@@ -6,7 +6,7 @@
 
 (defn get-item [k]
   (if (local-storage?)
-    (. js/localStorage (getItem k))))
+    (.getItem js/localStorage (if (keyword? k) (name k) k))))
 
 (defn get-clojure [k]
   (if-let [item (get-item k)]
@@ -15,7 +15,7 @@
 (defn set-item! [k v]
   (if (local-storage?)
     (let [item (if (string? v) v (pr-str v))]
-      (.setItem js/localStorage k item))))
+      (.setItem js/localStorage (if (keyword? k) (name k) k) item))))
 
 (defn conj-item!
   "Assume localStorage for key k is (or should become) a vector and conj value v"
